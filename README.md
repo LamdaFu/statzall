b81f26e9765f4641ccb8b70a9ac838c2474205f7
Matt Pouttu-Clarke <voidpointer101@gmail.com>
2015-12-20 08:19:37 -0700

    Added CDAP support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # λ福 - statzall
-Streaming stats designed to handle multiple field datatypes (unions).
+Streaming Data Science Primitives supporting multiple data types in the same field (unions).
 
 Please see the [Wiki](../../wiki) for more information.
---
+++ statzall-cdap/src/main/java/statzall/cdap/StreamStatApp.java
@@ -0,0 +1,22 @@
+package statzall.cdap;
+
+import co.cask.cdap.api.app.AbstractApplication;
+import co.cask.cdap.api.data.stream.Stream;
+import co.cask.cdap.api.dataset.lib.KeyValueTable;
+
+@SuppressWarnings("rawtypes")
+public class StreamStatApp extends AbstractApplication {
+	static final String APP_NAME = "StreamStatApp";
+	static final String STREAM_NAME = "statStream";
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void configure() {
+		setName(APP_NAME);
+		Stream stream = new Stream(STREAM_NAME);
+		addStream(stream);
+		createDataset("streamStat", KeyValueTable.class);
+		addFlow(new StreamStatFlow());
+		addService(StreamStatHttpHandler.NAME, new StreamStatHttpHandler());
+	}
+}
